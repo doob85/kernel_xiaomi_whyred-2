d2f8934f9fc286d9d9fa9c82bdd556cd4de286c3
ktommy91 <kemenesi.tamas@gmail.com>
2019-01-11 21:21:45 +0100

    kangaroox bringup build scripts from @psndna88

---
+++ gcc-miui.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+ANYKERNEL_DIR=/home/tamas/Desktop/Anykernel2-EAS-rebase
+ZIP_NAME="KangarooX-MIUI-EAS-r3.4-test"
+export ARCH=arm64
+export CROSS_COMPILE="/home/tamas/Desktop/tc/br/bin/aarch64-buildroot-linux-gnu-"
+export CROSS_COMPILE_ARM32=/home/tamas/Desktop/tc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_VERSION="4"
+mkdir -p out
+
+echo ""
+echo " Cross-compiling KangarooX kernel for whyred..."
+echo ""
+
+cd $KERNELDIR/
+
+if [ ! -f $KERNELDIR/out/.config ];
+then
+    make defconfig O=out ARCH=arm64 kangaroox-miui-eas_defconfig
+fi
+
+
+make -j4 O=out ARCH=arm64
+
+rm -f ${ANYKERNEL_DIR}/Image.gz*
+rm -f ${ANYKERNEL_DIR}/zImage*
+rm -f ${ANYKERNEL_DIR}/dtb*
+cp $KERNELDIR/out/arch/arm64/boot/Image.*-dtb ${ANYKERNEL_DIR}/zImage-dtb
+rm -rf ${ANYKERNEL_DIR}/modules/system/vendor/lib/modules
+mkdir -p ${ANYKERNEL_DIR}/modules/system/vendor/lib/modules
+cd ${ANYKERNEL_DIR}
+rm *.zip
+zip -r9 ${ZIP_NAME}.zip * -x README ${ZIP_NAME}.zip
+echo ""
+echo "KangarooX has been built for whyred !!!"
+echo ""
+

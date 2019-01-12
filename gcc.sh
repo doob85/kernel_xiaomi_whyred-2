1fed12dee5041410e23230a7947a66bdaa74026f
ktommy91 <kemenesi.tamas@gmail.com>
2019-01-12 20:43:13 +0100

    update the build scripts

--- gcc.sh
+++ gcc.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
 ANYKERNEL_DIR=/home/tamas/Desktop/Anykernel2-EAS-rebase
-ZIP_NAME="KangarooX-EAS-r3.4-test"
-export ARCH=arm64
-export CROSS_COMPILE="/home/tamas/Desktop/tc/br/bin/aarch64-buildroot-linux-gnu-"
+ZIP_NAME="KangarooX-EAS-r3.4-test2"
+export CROSS_COMPILE="/home/tamas/Desktop/tc/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-"
 export CROSS_COMPILE_ARM32=/home/tamas/Desktop/tc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_VERSION="4"
+# export KBUILD_BUILD_VERSION="4"
 mkdir -p out
-
 echo ""
 echo " Cross-compiling KangarooX kernel for whyred..."
 echo ""
@@ -18,16 +16,15 @@ cd $KERNELDIR/
 
 if [ ! -f $KERNELDIR/out/.config ];
 then
-    make defconfig O=out ARCH=arm64 kangaroox-eas_defconfig
+   make O=out ARCH=arm64 kangaroox-eas_defconfig
 fi
-
 
 make -j4 O=out ARCH=arm64
 
 rm -f ${ANYKERNEL_DIR}/Image.gz*
 rm -f ${ANYKERNEL_DIR}/zImage*
 rm -f ${ANYKERNEL_DIR}/dtb*
-cp $KERNELDIR/out/arch/arm64/boot/Image.*-dtb ${ANYKERNEL_DIR}/zImage-dtb
+mv $KERNELDIR/out/arch/arm64/boot/Image.*-dtb ${ANYKERNEL_DIR}/zImage-dtb
 rm -rf ${ANYKERNEL_DIR}/modules/system/vendor/lib/modules
 mkdir -p ${ANYKERNEL_DIR}/modules/system/vendor/lib/modules
 cd ${ANYKERNEL_DIR}
